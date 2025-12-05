ce3dc66805a9a7359aad93f4c0a94cd1f8a5e4f5
Dino Wei <dreamer@dcsh.tp.edu.tw>
2025-12-05 11:45:18 +0800

    完成投票主題列表、查看投票主題、投票功能

--- default/urls.py
+++ default/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from .views import poll_list, PollList, PollView, PollVote
 
 urlpatterns = [
+    # path("", poll_list),
+    # path("list", PollList.as_view(), name='poll_list'),
+    path("", PollList.as_view(), name='poll_list'),
+    path("<int:pk>/", PollView.as_view(), name='poll_view'), 
+    path('<int:oid>/vote/', PollVote.as_view(), name='poll_vote'),
 ]
